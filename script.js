6e82398c3098e816f0de2e846174bc7cd7541b53
Nathan Michael Allen <prodnathanallen@gmail.com>
2025-04-14 15:11:11 -0400

    scripting

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  const div = document.getElementById("last-updated");
+  const modified = new Date(document.lastModified);
+
+  const mm = String(modified.getMonth() + 1).padStart(2, '0');
+  const dd = String(modified.getDate()).padStart(2, '0');
+  const yy = String(modified.getFullYear()).slice(-2);
+
+  div.textContent = `Last updated [${mm}/${dd}/${yy}]`;
+</script>
