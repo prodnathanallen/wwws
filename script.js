4e2c3a3a8db89b573baa8902ce08cd930ad56544
Nathan Michael Allen <prodnathanallen@gmail.com>
2025-04-14 15:12:57 -0400

    modified date script working./gp.sh scripting

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-<script>
-  const div = document.getElementById("last-updated");
-  const modified = new Date(document.lastModified);
+const div = document.getElementById("last-updated");
+const modified = new Date(document.lastModified);
 
-  const mm = String(modified.getMonth() + 1).padStart(2, '0');
-  const dd = String(modified.getDate()).padStart(2, '0');
-  const yy = String(modified.getFullYear()).slice(-2);
+const mm = String(modified.getMonth() + 1).padStart(2, '0');
+const dd = String(modified.getDate()).padStart(2, '0');
+const yy = String(modified.getFullYear()).slice(-2);
 
-  div.textContent = `Last updated [${mm}/${dd}/${yy}]`;
-</script>
+div.textContent = `Last updated [${mm}/${dd}/${yy}]`;
